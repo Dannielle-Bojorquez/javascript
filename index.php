1403249b26c1464aaded18d9129266a854d9a8e2
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-08-09 20:14:33 -0600

    first ajax boo yeah!

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 <html>
 	<head>
 		<title>Javascript</title>
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
 		<script src="script.js">
 		</script>
+
 		<link rel="stylesheet" href="jscript.css">
+
 	</head>
 	<body>
 		<form>
@@ -15,7 +20,24 @@
 			<p>First name:</p><input type="text" name="firstName" onchange="firstName">
 			<p>Last name:</p><input type="text" name="lastName" onchange="lastName">
 <button type="submit">Lets Go!</button>
-
 		</form>
+
+		<h2>Pirate's</h2>
+		<button id="pButton">
+			Arg!
+		</button>
+		<div id="pOutput">
+			<!--ipsum Lute! -->
+		</div>
+		<p>The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg!</p>
+
+		<!-- adding ajax call button -->
+		<h3>Lets make BACON!</h3>
+		<button id="crispyButton">
+			Make it Crispy!
+		</button>
+		<div id="crispyOutput">
+			<!--ipsum here please-->
+		</div>
 	</body>
 </html>
--- script.js
+++ script.js
@@ -7,7 +7,19 @@ function changeBack() {
 	document.getElementById("circle").style.backgroundColor="chartreuse";
 	document.getElementById("circle").style.color="black";
 }
+//trying to change my font?!
+
+
 //adding ajax to the playing field
 $(document).ready(function() {
-
+	$("#crispyButton").click(function(){
+		$.ajax({
+			method: "GET" ,
+			url: "https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=htm"}).done(function(reply) {
+			$("#crispyOutput").html(reply);
+			console.log("you have crispy bacon!");
+		}
+		)
+	}
+	)
 });
